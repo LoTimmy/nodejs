3430305c3d011edaf5c2f9c52631649236527b0c
Timmy <lotimmy@users.noreply.github.com>
2017-08-29 00:33:44 +0800

    added chinese_parseInt

---
+++ chinese_parseInt/chinese_parseInt.js
@@ -0,0 +1,7 @@
+'use strict';
+const chinese_parseInt = require('chinese-parseint');
+const log = console.log;
+
+log(chinese_parseInt('六八九萬'));
+log(chinese_parseInt('一千零一十'));
+log(chinese_parseInt('二十四万二'));
