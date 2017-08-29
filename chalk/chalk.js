a3f47e191a6d2f8019c28d2ad22d83ee0e6aa221
Timmy <lotimmy@users.noreply.github.com>
2017-08-29 14:22:58 +0800

    added chalk

---
+++ chalk/chalk.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const chalk = require('chalk');
+
+console.log(chalk.blue('Hello world!'));
+
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+
