23e32b565c3a23ac498d88a80b6c5d23dee1a2e5
Timmy <lotimmy@users.noreply.github.com>
2017-09-04 10:14:21 +0800

    added regex.js

---
+++ regex.js
@@ -0,0 +1,16 @@
+"use strict";
+var string = 'foo💩bar';
+var match = string.match(/foo(.)bar/u);
+console.log(match[1]);
+
+var regex = /[\u{1F4A9}-\u{1F4AB}]/u;
+// The following regex is equivalent. 
+// var alternative = /[💩-💫]/u;
+console.log([
+  regex.test('a'), // false 
+  regex.test('💩'), // true 
+  regex.test('💪'), // true 
+  regex.test('💫'), // true 
+  regex.test('💬') // false 
+]);
+
