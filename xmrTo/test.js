4c02570f38d2de8280d1f9c345f467b1fcc6231a
Timmy <lotimmy@users.noreply.github.com>
2017-09-07 23:06:50 +0800

    updated xmrTo

--- xmrTo/test.js
+++ xmrTo/test.js
@@ -1,18 +1,20 @@
-'use strict';
-const co = require('co');
-const xmr2btc = require('xmrto-api');
+"use strict";
+const co = require("co");
+const xmr2btc = require("xmrto-api");
 const xmrTo = new xmr2btc();
-const numeral = require('numeral');
+const numeral = require("numeral");
 const MaiCoin = require("MaiCoin");
 const log = console.log;
 
-xmrTo.getParams()
-  .then(function(params) {
-    co(function*() {
-      let uY8ao6So = 0.657545525797 * params.price;
-      log(uY8ao6So);
-      let oonga9Oh = yield MaiCoin.getEthereumPrice('twd');
-      let fie2HahP = numeral(0.22766623 * oonga9Oh.raw_sell_price).divide(100000).value();
-      log(fie2HahP);
-    });
+xmrTo.getParams().then(params => {
+  co(function*() {
+    log(params.price);
+    let uY8ao6So = 0.657545525797 * params.price;
+    log(uY8ao6So);
+    let oonga9Oh = yield MaiCoin.getBitcoinPrice("twd");
+    let fie2HahP = numeral(uY8ao6So * oonga9Oh.raw_sell_price)
+      .divide(100000)
+      .value();
+    log(fie2HahP);
   });
+});
