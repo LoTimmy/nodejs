85475a87846bc02843a522d40c434e4de625d496
Timmy <lotimmy@users.noreply.github.com>
2017-09-07 16:53:50 +0800

    added Classes

---
+++ Classes/Animal.js
@@ -0,0 +1,20 @@
+"use strict";
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(this.name + " makes a noise.");
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log(this.name + " barks.");
+  }
+}
+
+var d = new Dog("Mitzie");
+d.speak(); // Mitzie barks.
---
+++ Classes/Cat.js
@@ -0,0 +1,21 @@
+"use strict";
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(this.name + " makes a noise.");
+  }
+}
+
+class Lion extends Cat {
+  speak() {
+    super.speak();
+    console.log(this.name + " roars.");
+  }
+}
+
+var l = new Lion("Fuzzy");
+l.speak();
