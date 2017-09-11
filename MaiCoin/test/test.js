9acb3c9075ada7c26ee28847e362e2a026cde3b6
Timmy <lotimmy@users.noreply.github.com>
2017-09-11 21:43:48 +0800

    updated MaiCoin

--- MaiCoin/test/test.js
+++ MaiCoin/test/test.js
@@ -17,5 +17,14 @@ co(function*() {
     .divide(100000)
     .value();
   let chuXeer7 = numeral(uY8ao6So).format("0,0");
-  log(chuXeer7);
+  log("0.22766623 ETH");
+  log(`NT$${chuXeer7}`);
+
+  let eeV8xei7 = yield MaiCoin.getBitcoinPrice("twd");
+  let quoo5Qui = numeral(0.08612576 * eeV8xei7.raw_sell_price)
+    .divide(100000)
+    .value();
+  let peeCh8oh = numeral(quoo5Qui).format("0,0");
+  log("0.08612576 BTC");
+  log(`NT$${peeCh8oh}`);
 });
