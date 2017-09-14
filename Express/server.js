9e6c98aa9e0604140281abd26bb7d348a202c53e
Timmy <lotimmy@users.noreply.github.com>
2017-09-14 14:35:28 +0800

    updated Express

--- Express/server.js
+++ Express/server.js
@@ -1,19 +1,30 @@
-'use strict';
+"use strict";
 // 导入所需模块
 const express = require("express");
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
+const morgan = require("morgan");
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
 
 const app = express();
 
-app.use("/", express.static("public"));
-app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
+app.use(morgan("combined", { stream: accessLogStream }));
 
-app.get('/example/a', function (req, res) {
+app.use("/", express.static("public"));
+app.use(
+  "/bower_components",
+  express.static(path.join(__dirname, "bower_components"))
+);
+
+app.get("/example/a", function(req, res) {
   res.send(req.hostname);
   // res.send('Hello from A!');
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Example app listening on port 3000!");
 });
-
