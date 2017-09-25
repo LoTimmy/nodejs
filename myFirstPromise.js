6a6bb7e454d1ec0d873d05d47b713a7010398494
Timmy <lotimmy@users.noreply.github.com>
2017-09-25 23:39:58 +0800

    added myFirstPromise.js

---
+++ myFirstPromise.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+let myFirstPromise = new Promise((resolve, reject) => {
+  setTimeout(function(){
+    resolve("Success!");
+  }, 1000);
+});
+*/
+
+let myFirstPromise = new Promise((resolve, reject) =>
+  setTimeout(() => resolve("Success!"), 1000)
+);
+
+myFirstPromise.then(successMessage => {
+  console.log("Yay! " + successMessage);
+});
