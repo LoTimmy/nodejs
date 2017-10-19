2a797dd6df49d2bacb9d2a4aa251f4fd9b2d7328
Timmy <lotimmy@users.noreply.github.com>
2017-10-19 14:32:00 +0800

    updated Bei7eino/index.js

--- Bei7eino/index.js
+++ Bei7eino/index.js
@@ -1,20 +1,24 @@
 "use strict";
-const co = require('co');
+const co = require("co");
 const request = require("co-request");
-const queryString = require('query-string');
-const urljoin = require('url-join');
+const queryString = require("query-string");
+const urljoin = require("url-join");
 const stringify = require("json-stringify-pretty-compact");
 
 (function(exports) {
   // 公司基本資料
   exports.Aiphie8i = co.wrap(function*(Business_Accounting_NO) {
     let obj = {
-      $format: 'json',
+      $format: "json",
       // $format: 'xml',
-      $filter: 'Business_Accounting_NO eq ' + Business_Accounting_NO // 統一編號
+      $filter: "Business_Accounting_NO eq " + Business_Accounting_NO // 統一編號
     };
     let stringified = queryString.stringify(obj);
-    let fullUrl = urljoin('http://data.gcis.nat.gov.tw/od/data/api', '5F64D864-61CB-4D0D-8AD9-492047CC1EA6', '?' + stringified);
+    let fullUrl = urljoin(
+      "http://data.gcis.nat.gov.tw/od/data/api",
+      "5F64D864-61CB-4D0D-8AD9-492047CC1EA6",
+      "?" + stringified
+    );
     let thah5Tee = yield request.get({
       url: fullUrl,
       json: false
@@ -37,12 +41,16 @@ const stringify = require("json-stringify-pretty-compact");
   // 所營事業資料
   exports.Bay6hohz = co.wrap(function*(Business_Accounting_NO) {
     let obj = {
-      $format: 'json',
+      $format: "json",
       // $format: 'xml',
-      $filter: 'Business_Accounting_NO eq ' + Business_Accounting_NO
+      $filter: "Business_Accounting_NO eq " + Business_Accounting_NO
     };
     let stringified = queryString.stringify(obj);
-    let fullUrl = urljoin('http://data.gcis.nat.gov.tw/od/data/api', '236EE382-4942-41A9-BD03-CA0709025E7C', '?' + stringified);
+    let fullUrl = urljoin(
+      "http://data.gcis.nat.gov.tw/od/data/api",
+      "236EE382-4942-41A9-BD03-CA0709025E7C",
+      "?" + stringified
+    );
     let thah5Tee = yield request.get({
       url: fullUrl,
       json: false
@@ -54,12 +62,16 @@ const stringify = require("json-stringify-pretty-compact");
   // 董監事資料
   exports.Cie5za2o = co.wrap(function*(Business_Accounting_NO) {
     let obj = {
-      $format: 'json',
+      $format: "json",
       // $format: 'xml',
-      $filter: 'Business_Accounting_NO eq ' + Business_Accounting_NO
+      $filter: "Business_Accounting_NO eq " + Business_Accounting_NO
     };
     let stringified = queryString.stringify(obj);
-    let fullUrl = urljoin('http://data.gcis.nat.gov.tw/od/data/api', '4E5F7653-1B91-4DDC-99D5-468530FAE396', '?' + stringified);
+    let fullUrl = urljoin(
+      "http://data.gcis.nat.gov.tw/od/data/api",
+      "4E5F7653-1B91-4DDC-99D5-468530FAE396",
+      "?" + stringified
+    );
     let thah5Tee = yield request.get({
       url: fullUrl,
       json: false
@@ -75,13 +87,17 @@ const stringify = require("json-stringify-pretty-compact");
 
   exports.DoB7coo4 = co.wrap(function*(Responsible_Name) {
     let obj = {
-      $format: 'json',
+      $format: "json",
       // $format: 'xml',
-      $filter: 'Responsible_Name eq ' + Responsible_Name
+      $filter: "Responsible_Name eq " + Responsible_Name
     };
 
     let stringified = queryString.stringify(obj);
-    let fullUrl = urljoin('http://data.gcis.nat.gov.tw/od/data/api', '4B61A0F1-458C-43F9-93F3-9FD6DA5E1B08', '?' + stringified);
+    let fullUrl = urljoin(
+      "http://data.gcis.nat.gov.tw/od/data/api",
+      "4B61A0F1-458C-43F9-93F3-9FD6DA5E1B08",
+      "?" + stringified
+    );
     let thah5Tee = yield request.get({
       url: fullUrl,
       json: false
@@ -89,6 +105,4 @@ const stringify = require("json-stringify-pretty-compact");
 
     return Promise.resolve(stringify(JSON.parse(thah5Tee.body)));
   });
-
-}(this));
-
+})(this);
