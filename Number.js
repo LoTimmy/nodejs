d285fac629f2ae60c6e405f7034c2687c62a56ff
Timmy <lotimmy@users.noreply.github.com>
2017-10-31 13:28:03 +0800

    updated Number.js

--- Number.js
+++ Number.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 // const biggestInt = 9007199254740991;
 // const biggestInt = Number.MAX_SAFE_INTEGER
 const biggestInt = Math.pow(2, 53) - 1;
@@ -19,7 +19,7 @@ console.log(biggestNum);
 console.log(smallestNum);
 console.log(infiniteNum);
 
-var d = new Date('December 17, 1995 03:24:00');
+var d = new Date("December 17, 1995 03:24:00");
 console.log(Number(d));
 
 console.log(Number.isSafeInteger(Math.pow(2, 53) - 1));
